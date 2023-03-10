df1685417e8ca9f7f38b3ff1e4cead5c23425785
Ali Najibi <58134947+anajibi@users.noreply.github.com>
2023-03-10 16:06:14 +0330

    this is just a simple modification to create a merge conflict

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,12 +3,13 @@ import {Router} from 'express';
 import Paths from './constants/Paths';
 import logTypesRouter from '@src/routes/LogTypeRouter';
 import genSettingsRouter from '@src/routes/GenSettingsRouter';
-
+import {Knex} from "knex";
 
 // **** Variables **** //
 
 const apiRouter = Router();
 
+// This is a modification to creat a merge conflict, it is not for any other purpose
 // **** Export default **** //
 
 
